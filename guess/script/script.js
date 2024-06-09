9a0ee7699d86c4c7f387808f224b621e77cc3ded
Imposter <biplovdhakal@gmail.com>
2024-06-09 22:45:19 +0545

    biplov-portfoli

---
+++ guess/script/script.js
@@ -0,0 +1,43 @@
+
+let button = document.getElementById("button");
+let picture = document.getElementById("picture");
+let picture1 = document.getElementById("picture1");
+
+let b = Math.floor(Math.random()*100);
+let t=10
+button.addEventListener('click', () => {
+    let a = +document.getElementById("guess_box").value;
+    let right = document.getElementById("right");
+    let guessed = document.getElementById("guessed");
+    let tries = document.getElementById("lives");
+    
+    if (t>=1){
+    if (a === b) {
+        guessed.innerHTML = guessed.innerHTML + `${a} ,`;
+        right.innerText = "You guessed the correct number!";
+        picture.style.display="block";
+        right.style.display = "block";
+    }
+    else if (a < b) {
+        guessed.innerHTML = guessed.innerHTML + ` ${a} ,`;
+        right.innerText = "Hint: the number is greater";
+        right.style.display = "block";
+        t=t-1;
+        tries.innerText=`tries left :${t}`
+
+
+    } else if (a > b) {
+        guessed.innerHTML = guessed.innerHTML + ` ${a}, `;
+        right.innerText = "Hint: the number is smaller";
+        right.style.display = "block";
+        t=t-1;
+        tries.innerText=`tries left :${t}`
+    } {
+
+    }}
+    else{
+        right.innerText = "game over!";
+        picture1.style.display="block";
+
+    }
+});
