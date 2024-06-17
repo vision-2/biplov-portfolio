1e5cba361ea2606be3194934827161a6cc395eac
Imposter <biplovdhakal@gmail.com>
2024-06-17 15:04:26 +0545

    biplov-portfolio

---
+++ JavaScript/main.js
@@ -0,0 +1,21 @@
+let hiddenElements = document.getElementsByClassName("hidden");
+let viewDiv = document.getElementById("read_more");
+
+viewDiv.addEventListener('click', () => {
+    let allHidden = true;
+
+    for (let i = 0; i < hiddenElements.length; i++) {
+        if (hiddenElements[i].style.display === "none" || hiddenElements[i].style.display === "") {
+            hiddenElements[i].style.display = "block";
+            viewDiv.innerText="View Less";
+            viewDiv.style.color="white";
+            allHidden = false;
+        } else {
+            hiddenElements[i].style.display = "none";
+            viewDiv.innerText="View More";
+            viewDiv.style.color="white";
+
+
+        }
+    }
+});
